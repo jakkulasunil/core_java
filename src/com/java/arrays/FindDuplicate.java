437ab357dcf083fbc993b0187122069592a94a8f
suniljakkula <sunij8911@gmail.com>
2025-11-17 08:38:57 +0530

    find duplicate

---
+++ src/com/java/arrays/FindDuplicate.java
@@ -0,0 +1,22 @@
+package com.java.arrays;
+
+public class FindDuplicate {
+	static int findDuplicateElement(int[] arr) {
+		int n = arr.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (arr[i] == arr[j])
+					return arr[i];
+			}
+		}
+		return -1;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {1,3,2,3,4};
+		System.out.println(findDuplicateElement(arr));
+	}
+
+}
