a555c99d3c829b9889ce698ee7b30765832ed6b8
suniljakkula <sunij8911@gmail.com>
2025-11-17 07:12:36 +0530

    missing number

---
+++ src/com/java/arrays/MissingAndRepeatingNumber.java
@@ -0,0 +1,42 @@
+package com.java.arrays;
+
+import java.util.ArrayList;
+
+public class MissingAndRepeatingNumber {
+
+	static ArrayList<Integer> findTwoElement(int[] arr) {
+		
+		int n=arr.length;
+		
+		int[] freq=new int[n+1];
+		int repeating = -1;
+		int missing = -1;
+		
+		for(int i=0;i<n;i++) {
+			freq[arr[i]]++;
+		}
+		
+		for(int i=1;i<=n;i++) {
+			if(freq[i]==0)
+				missing = i;
+			else if(freq[i]==2)
+				repeating = i;
+		}
+		
+		ArrayList<Integer> result=new ArrayList<>();
+		result.add(missing);
+		result.add(repeating);
+		
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 3, 1, 3 };
+		ArrayList<Integer> ans = findTwoElement(arr);
+
+		System.out.println(ans.get(0) + " " + ans.get(1));
+	}
+
+}
