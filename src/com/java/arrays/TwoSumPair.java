0f4c8116ceca4700e5a50b3cd88c987b55dbf394
suniljakkula <sunij8911@gmail.com>
2025-11-18 06:39:40 +0530

    two sum pair

---
+++ src/com/java/arrays/TwoSumPair.java
@@ -0,0 +1,29 @@
+package com.java.arrays;
+
+public class TwoSumPair {
+
+	static boolean twoSum(int[] arr, int target) {
+		int n = arr.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (arr[i] + arr[j] == target) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 0, -1, 2, -3, 1 };
+        int target = -2;
+      
+        if (twoSum(arr, target))
+            System.out.println("true");
+        else
+            System.out.println("false");
+
+	}
+
+}
