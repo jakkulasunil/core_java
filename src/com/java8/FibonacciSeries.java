c1c1ac8dc830dd093116eea8f95480b1d13e0d8d
suniljakkula <sunij8911@gmail.com>
2025-11-21 06:21:05 +0530

    fibonacci series

---
+++ src/com/java8/FibonacciSeries.java
@@ -0,0 +1,14 @@
+package com.java8;
+
+import java.util.stream.Stream;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Stream.iterate(new int[] { 0, 1 }, f -> new int[] { f[1], f[0] + f[1] }).limit(10).map(f -> f[0])
+				.forEach(i -> System.out.print(i + ", "));
+	}
+
+}
