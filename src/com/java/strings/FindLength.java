c5b865a374861ad0e0752e5f675e7413d856dde8
suniljakkula <sunij8911@gmail.com>
2025-11-29 06:18:31 +0530

    length of a String

---
+++ src/com/java/strings/FindLength.java
@@ -0,0 +1,13 @@
+package com.java.strings;
+
+public class FindLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s="Hello";
+		System.out.println(s.length());
+
+	}
+
+}
