ef447c552db05907f7f5c363c9d9877348615be2
suniljakkula <sunij8911@gmail.com>
2025-11-29 06:45:55 +0530

    reverse String

---
+++ src/com/java/strings/ReverseString.java
@@ -0,0 +1,15 @@
+package com.java.strings;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Hello";
+		StringBuilder res = new StringBuilder();
+		for (int i = s.length() - 1; i >= 0; i--) {
+			res.append(s.charAt(i));
+		}
+		System.out.println(res);
+	}
+
+}
